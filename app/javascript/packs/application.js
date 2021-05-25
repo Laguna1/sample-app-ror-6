4c6e06f5ca8aa28d35c8757920b7f02d5d2921c9
Laguna1 <lagunazoomarket@gmail.com>
2021-05-26 02:17:36 +0300

    Add bootstrap & font-awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "@fortawesome/fontawesome-free/js/all";
+import "@fortawesome/fontawesome-free/css/all";
 
 Rails.start()
 Turbolinks.start()
