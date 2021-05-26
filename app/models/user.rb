5d5406ddf863dc14fcab95c28ae3a98552159c41
Laguna1 <lagunazoomarket@gmail.com>
2021-05-26 21:12:31 +0300

    Add password_digest & update validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_secure_password
   before_save { self.email = email.downcase }
 
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
 end
